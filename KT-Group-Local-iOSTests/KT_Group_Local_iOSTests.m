f768a2cbb354b2ece9090569f42a1d2e0920d59a
cetauri <cetauri@gmail.com>
2013-03-13 11:47:08 +0900

    Initial commit

---
+++ KT-Group-Local-iOSTests/KT_Group_Local_iOSTests.m
@@ -0,0 +1,32 @@
+//
+//  KT_Group_Local_iOSTests.m
+//  KT-Group-Local-iOSTests
+//
+//  Created by cetauri on 13. 3. 13..
+//  Copyright (c) 2013년 KT. All rights reserved.
+//
+
+#import "KT_Group_Local_iOSTests.h"
+
+@implementation KT_Group_Local_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KT-Group-Local-iOSTests");
+}
+
+@end
