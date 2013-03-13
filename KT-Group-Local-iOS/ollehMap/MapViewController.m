dd18307c76802f8158b1b0a6c80007076b9d7981
Gyuchan Jeon- Corp <jeon.gyuchan@kt.com>
2013-03-13 18:25:44 +0900

    핀 기준 원형그리기

--- KT-Group-Local-iOS/ollehMap/MapViewController.m
+++ KT-Group-Local-iOS/ollehMap/MapViewController.m
@@ -35,10 +35,10 @@
     mapView.delegate = self;
     [mapView startMapService:@"8100D6CF" UserKey:@"TBFFA463318A46"];
     
-    [mapView setZoomLevel:11];
-    [mapView setMapType:KMapTypeStandard];
-
-    [mapView setCenterCoordinate:CoordMake(949118.568, 1950999.45)];
+    [mapView setZoomLevel:10];
+    //[mapView setMapType: KMapTypeHybrid];
+    [mapView setCenterCoordinate: CoordMake(949118.568, 1950999.45)];
+    [mapView setShowUserLocation: UserLocationCompassTrace];
     
     //핀 설정하기
     MarkerOverlay* overlay = [[MarkerOverlay alloc] initWithType:0];
@@ -47,8 +47,16 @@
     overlay.title = @"모두 친구가 되는 곳. 홍대";
     overlay.canShowBalloon = YES;
     
+    CircleOverlay* circleOverlay = [[CircleOverlay alloc] initWithCoord:mapView.centerCoordinate Radius:50.0];
+    circleOverlay.delegate = self;
+    circleOverlay.strokeColor = [[UIColor colorWithRed:0.5 green:0.5 blue:1.0 alpha:0.9]CGColor];
+    circleOverlay.lineWidth = 5;
+    circleOverlay.lineType = kLineType_Dash;
+    circleOverlay.fillColor = [[UIColor colorWithRed:0.7 green:0.7 blue:1.0 alpha:0.4]CGColor];
+    
     // MarkerOverlay 추가
     [mapView addOverlay:overlay];
+    [mapView addOverlay:circleOverlay];
     [self.view addSubview:mapView];
     NSLog(@"check: %d", [mapView checkStartMapService]);
 }
